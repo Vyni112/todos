a156045dd3605a7f92fd70f2ceed86c4810b326f
Vyni112 <143663850+Vyni112@users.noreply.github.com>
2023-11-19 18:56:56 -0300

    Update form.js

--- docs/trabalho/form.js
+++ docs/trabalho/form.js
@@ -1,18 +1,16 @@
-
 $(document).ready(function() {
     $('.telefone').mask('(99) 99999-9999');
 });
 
-
-function  validarFormulario() {
+function validarFormulario() {
     var nome = document.getElementById('nome').value;
     var idade = document.getElementById('idade').value;
     var genero = document.querySelector('input[name="genero"]:checked');
     var interesses = document.querySelectorAll('input[name="interesses"]:checked');
-    var mensagem = document.getElementById('mensagem').value;
+    // Removido o trecho que tenta acessar um elemento com o ID 'mensagem'
     var telefone = document.getElementById('telefone').value;
 
-    if (nome.trim() === '' || idade.trim() === '' || genero === null || interesses.length === 0 || mensagem.trim() === '' || telefone.trim() === '') {
+    if (nome.trim() === '' || idade.trim() === '' || genero === null || interesses.length === 0 || telefone.trim() === '') {
         alert('Por favor, preencha todos os campos.');
     } else {
         alert('Formulário enviado com sucesso!');
